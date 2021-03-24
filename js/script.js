92c5da3577b2028f7fd826f5ca37a356b7db53d1
Alfredo Ficocelli <alfredo.ficocelli@gmail.com>
2021-03-24 15:10:51 +0100

    add. css

--- js/script.js
+++ js/script.js
@@ -8,8 +8,23 @@ var app = new Vue ({
     message: "Ciao classe 27",
 
     coverImg: "https://miro.medium.com/max/3840/1*U-R58ahr5dtAvtSLGK2wXg.png",
-  }
+  },
 
+  methods: {
+
+    myFunction: function() {
+
+      if ( this.coverImg == "https://miro.medium.com/max/3840/1*U-R58ahr5dtAvtSLGK2wXg.png" ){
+
+        return this.coverImg = "https://miro.medium.com/max/2400/1*iNw33pBEYZIAoE9oHdhLLw.jpeg";
+
+      } else{
+        return this.coverImg = "https://miro.medium.com/max/3840/1*U-R58ahr5dtAvtSLGK2wXg.png";
+      }
+
+    }
+
+  }
 
 
 });
