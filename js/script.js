a861803148fe2608853d7410afed9af781fd98ca
Alfredo Ficocelli <alfredo.ficocelli@gmail.com>
2021-03-24 14:50:54 +0100

    add. img

---
+++ js/script.js
@@ -0,0 +1,15 @@
+var app = new Vue ({
+
+  el: '#root',
+
+  data: {
+
+
+    message: "Ciao classe 27",
+
+    coverImg: "https://miro.medium.com/max/3840/1*U-R58ahr5dtAvtSLGK2wXg.png",
+  }
+
+
+
+});
